7a4e2fa0ec4e5914b2668d7b478d7cd8b58f5bd9
Mahmut Kut <kutmahmut37@gmail.com>
2024-05-08 16:41:12 +0300

    Fix: deleted unused lines and deleted unnecessary spaces

--- src/color_detec_passing_door/src/utils.py
+++ src/color_detec_passing_door/src/utils.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 
-
 def create_mask(color_name, hsv):  #'create_mask' fonksiyonu 2 adet parametre aliyor ve 1 tane cikti veriyor
     if color_name == "blue":
         lowwer_color = np.array([94, 80, 2])
@@ -29,50 +28,18 @@ def empty(x):
 
 def detect_shape(cnt,frame):
     area = cv2.contourArea(cnt)  # contourlarin alanini buluyoruz
-    epsilon = 0.02 * cv2.arcLength(
-        cnt, True
-    )  # epsilon degeri buluyoruz sabit denklem
+    epsilon = 0.02 * cv2.arcLength(cnt, True)  # epsilon degeri buluyoruz sabit denklem
     approx = cv2.approxPolyDP(cnt, epsilon, True)  # kose buluyoruz
-    if (
-        cv2.contourArea(cnt) > 600
-    ):  # buldugu ufak nesneleri almasin diye contour alaniyla beraber filtreliyoruz
+    if (cv2.contourArea(cnt) > 600):  # buldugu ufak nesneleri almasin diye contour alaniyla beraber filtreliyoruz
         x, y, w, h = cv2.boundingRect(cnt)
         x_center = int(x + (w / 2))  # X merkezini buluyoruz
         y_center = int(y + (h / 2))  # Y merkezini buluyoruz
-
         # cv2.circle(frame,(x_center,y_center),3,(0,0,255),-1) # merkezine bir nokta koyuyoruz
         # cv2.line(frame,(int(width/2),int(height/2)),(x_center,y_center),(0,0,0),2)  #goruntunun merkezinden nesnenin merkezine cizgi ciziyoruz
-
-        print(
-            len(approx)
-        )  # len(approx) sayesinde kose sayisi aliyoruz ve ona gore sekli algiliyoruz
+        print(len(approx))  # len(approx) sayesinde kose sayisi aliyoruz ve ona gore sekli algiliyoruz
         if len(approx) == 3:
-            cv2.putText(
-                frame,
-                "Ucgen",
-                (x_center, y_center),
-                cv2.FONT_HERSHEY_COMPLEX,
-                2,
-                (0),
-                2,
-            )
+            cv2.putText( frame, "Ucgen", (x_center, y_center), cv2.FONT_HERSHEY_COMPLEX, 2, (0), 2)
         if len(approx) == 4:
-            cv2.putText(
-                frame,
-                "Dortgen",
-                (x_center, y_center),
-                cv2.FONT_HERSHEY_COMPLEX,
-                2,
-                (0),
-                2,
-            )
+            cv2.putText( frame, "Dortgen", (x_center, y_center), cv2.FONT_HERSHEY_COMPLEX, 2, (0), 2)
         if len(approx) > 7:
-            cv2.putText(
-                frame,
-                "Cember",
-                (x_center, y_center),
-                cv2.FONT_HERSHEY_COMPLEX,
-                2,
-                (0),
-                2,
-            )
+            cv2.putText( frame, "Cember", (x_center, y_center), cv2.FONT_HERSHEY_COMPLEX, 2, (0), 2)
